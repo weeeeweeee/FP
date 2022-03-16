4073b721ef3086e8eb0c10dc921212482e350bb1
Сергей Чутчев <schutchev@gmail.com>
2022-03-16 20:41:02 +0300

    11 задание

---
+++ LR5.11/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let z = Console.ReadLine()
+    if z = "F#" || z = "Prolog" then
+        printf "Подлиза"
+    else if z = "1С" then
+        printf "Хороший язык, чем-то напоминает русский."
+    else
+        printf "Ну хотя бы не подлиза..."
+    0
