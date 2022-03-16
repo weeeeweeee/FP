820c4803a3f8f2491d84bf38bd3b64eec4441122
Сергей Чутчев <schutchev@gmail.com>
2022-03-16 21:13:14 +0300

    5 лр, 12 задание

---
+++ LR5.12/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+
+[<EntryPoint>]
+let main argv =
+    let read = Console.ReadLine()
+    let treat a =
+        if a = "F#" || a = "Prolog" then
+            "Подлиза.\n"
+        else if a = "1С" then
+            "Хороший язык, чем-то похож на русский.\n"
+        else
+            "Ну хотя бы не подлиза.\n"
+    let prnt a = printf "%s" a
+
+    (treat >> prnt) read // Работа с использованием оператора суперпозиции
+
+    prnt (treat read)
+    0
