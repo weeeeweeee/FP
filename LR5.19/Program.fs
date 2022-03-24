da4ad0f3dcb35e5d246cae47608745d7d302ce78
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-03-24 22:57:06 +0300

    5 лр, 19 задание, третий метод

--- LR5.19/Program.fs
+++ LR5.19/Program.fs
@@ -42,15 +42,42 @@ let rec NumberTraverse n f init predicate =
 let v2 x =
     NumberTraverse x (fun x y -> if x > y then x else y) -1 (fun x -> x % 3 <> 0)
 
+    
+
+let DivTraverse x (func : int -> int -> int) init =
+    let rec fr x y (func : int -> int -> int) init = 
+        if y > x then init
+        else
+            let i =
+                if x % y = 0 then
+                    func y init
+                else
+                    init
+            fr x (y + 1) func i
+    fr x 2 func init  
+// ^ - функция из 14 задания - обход делителей числа
+// (немного модифицированная, чтобы обход начинался
+// не с 1, а с 2, потому что иначе минимальный дели-
+// тель всегда будет равен 1, а числа, которое бы не
+// делилось на 1, не существует)
+
+let v3 x =
+    let min = DivTraverse x (fun x y -> if x < y then x else y) x
+    let max = NotCoprimeTraverse x (fun x y -> if x > y then x else y) -1 (fun x -> x % min <> 0)
+    let sum = NumberTraverse x (fun x y -> x + y) 0 (fun x -> x < 5) 
+    sum * max
+
 
 [<EntryPoint>]
 let main argv =
     printf "%d\n" (v1 20) // Ответ 10: 2 4 6 8 10 12 14 16 18 20
 
     printf "%d\n" (v2 2369) // Ответ, очевидно, 2
-    printf "%d\n" (v2 2368) // Ответ 8
-    printf "%d\n" (v2 369)  // Ответ -1, сигнализирующий о том, что 
-                            // числа, удовлетворяющего условию, нет
-
-
+    printf "%d\n" (v2 2368) // Ответ: 8
+    printf "%d\n" (v2 369)  // Ответ: -1, сигнализирующий о том, что 
+                            // числа, удовлетворяющего условию, не
+    
+    printf "%d\n" (v3 225)  // Ответ: 3 - наименьший делитель,
+                            // 220 - наибольшее невзаимнопростое
+                            // 220 * 4 = 880
     0
