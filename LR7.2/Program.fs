e01d0d5f9cbd522a342b8983ec81e6c85043299b
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-03-28 23:22:41 +0300

    7 лр, 2 (14) задание

---
+++ LR7.2/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Дан целочисленный массив и интервал a..b. Необходимо найти
+// количество элементов в этом интервале.
+
+
+
+let count list a b =
+    let checkInterval x =
+        x >= a && x <= b
+    List.fold (fun s x -> if checkInterval x then s + 1 else s ) 0 list
+
+[<EntryPoint>]
+let main argv =
+    let list = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+    let a = 4
+    let b = 9
+    printf "%d " (count list a b) 
+    0
