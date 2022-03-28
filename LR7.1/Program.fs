851b2b128f0bf7a12eb96ced77c37f1b4fa0463e
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-03-28 22:12:00 +0300

    7 лр, 1 (4) задание

---
+++ LR7.1/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Дан целочисленный массив. Вывести индексы массива в том
+// порядке, в котором соответствующие им элементы образуют
+// убывающую последовательность.
+
+let writeList list =
+    List.iter (printf "%d ") list
+    printf "\n"
+
+let func (list : int list) =
+    let indlist = List.indexed list
+    let sortlist = List.sortByDescending snd indlist
+    List.map fst sortlist
+
+[<EntryPoint>]
+let main argv =
+    let list = [4;2;1;3;5]
+    writeList (func list)
+    0
