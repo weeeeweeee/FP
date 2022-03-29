9eb392b8b44d14942712c1eff271e68d149a30e0
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-03-29 15:03:37 +0300

    7 лр, 6 (54) задание

---
+++ LR7.6/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Для введенного списка построить список из элементов, встречающихся
+// в исходном более трех раз.
+
+let writeList list =
+    List.iter (printf "%d ") list
+    printf "\n"
+
+let func list =
+    let lst = List.countBy id list
+    let lst = List.filter (fun x -> (snd x) > 3) lst
+    List.map fst lst
+
+[<EntryPoint>]
+let main argv =
+    let list = [1; 1; 1; 1; 2; 2; 2; 3; 3; 4; 4; 4; 4; 4; 4; 5; 6]
+    writeList (func list)
+    0
