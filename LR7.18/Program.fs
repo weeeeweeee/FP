9c1261248bd444c2f067225e7d4c6f3d95246f14
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-03-31 18:58:39 +0300

    7 лр, 18 (4) задание

---
+++ LR7.18/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Создать функцию фильтр для массива А [1; 2; 3; 4; 5; 6; 7;
+// 8; 9; 10; 11; 12] в котором элементы будут делиться на 3 без
+// остатка
+
+let writeArray arr =
+    Array.iter (printf "%d ") arr
+    printf "\n"
+
+let func arr =
+    Array.filter (fun x -> x % 3 = 0) arr
+
+[<EntryPoint>]
+let main argv =
+    let arr = [|1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12|]
+    let new_arr = func arr
+    writeArray new_arr
+    0
