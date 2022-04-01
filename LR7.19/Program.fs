efd5f36a6737cfc5bc542033207aa93d12ecf83d
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-04-01 18:07:27 +0300

    7 лр, 19 задание
    Попытался добавить функцию для подсчёта слов.

--- LR7.19/Program.fs
+++ LR7.19/Program.fs
@@ -6,6 +6,26 @@ let isPalyndrom str =
     let rev_str = Seq.rev str
     Seq.forall2 (fun x y -> x = y) str rev_str
 
+(*let DistSpaces (str:string) = 
+    let rec Dist (str:char list) (lastWasSpace : bool) (newstr:char list) =
+        match str with
+        h::t ->
+            if lastWasSpace && h = ' ' then
+                Dist t true newstr
+            else if not lastWasSpace && h = ' ' then
+                Dist t true newstr@[h]
+            else
+                Dist t false newstr@[h]
+        |[] ->
+            if lastWasSpace then
+                Seq.toList (Seq.rev (Seq.skip 1 (Seq.rev newstr)))
+            else
+                newstr
+
+    Seq.cast<char> (Dist (Seq.toList str) false [])*)
+
+let WordCount str =
+    Seq.fold (fun s x -> if x = ' ' then s + 1 else s) 1 str
 
 [<EntryPoint>]
 let main argv =
