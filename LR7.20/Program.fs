d16bf4b2fa5b5fa53703a9bd7add236492b12cbd
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-04-01 23:30:14 +0300

    7 лр, 20 задание
    Реализовал сортировку из первого пункта

--- LR7.20/Program.fs
+++ LR7.20/Program.fs
@@ -26,6 +26,17 @@ let ASCIICount (str:string) x =             // Количество символ
 let Averageforx (str:string) x =            // Среднее количество символов с одним ASCII-кодом
     float (ASCIICount str x) / float (String.length str)
 
+let SquareDeviation str1 str2 =             // Что-то страшное...
+    let aver1 = ASCIIAver str1
+    let len2 = String.length str2
+    let averageinstr2 = Averageforx str2
+    let D : float = Seq.fold (fun s x -> s + (averageinstr2 x)*((float x) - aver1)) (float 0) str2
+    sqrt D
+
+let sortwithsd str_list : string list =   // Собственно, сортировка
+    Seq.toList (Seq.sortBy (fun x -> SquareDeviation (Seq.head str_list) x) str_list)
+
+
 [<EntryPoint>]
 let main argv =
     
