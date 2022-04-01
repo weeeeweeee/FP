09d693ad30d76dc5aa9d74e1e126eaa1f3d30dd8
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-04-01 17:37:34 +0300

    7 лр, 19 задание
    Добавил функцию проверки строки на полином.

---
+++ LR7.19/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+let isPalyndrom str =
+    let rev_str = Seq.rev str
+    Seq.forall2 (fun x y -> x = y) str rev_str
+
+
+[<EntryPoint>]
+let main argv =
+    
+    0
