d6d1e20d34491e0a62079cf1016bca1557f8e419
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-05-28 13:59:02 +0300

    9.8

---
+++ LR9/LR9.8/LR9_8/Main.fs
@@ -0,0 +1,28 @@
+﻿module LR9_8
+
+
+open System
+open System.Windows.Forms
+open System.Windows.Forms
+open UI
+
+type StartupForm() as this = 
+    inherit Form1()
+    do
+        let txt = this.textBox1
+        this.button1.Click.Add <| fun _ ->
+                                        let mass = [|1..12|]
+                                        let ret = Array.filter (fun x -> x % 3 = 0) mass
+                                        txt.Text <- (ret |> Seq.map string |> String.concat ",")
+
+
+
+
+
+[<EntryPoint; STAThread>]
+let main argv =
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault(false)
+    use form = new StartupForm()
+    Application.Run(form)
+    0
