50a6f81907a813b730f092d6d3321410880eb3d4
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-05-28 13:36:31 +0300

    9.7

---
+++ LR9/LR9.7/LR9_7/Main.fs
@@ -0,0 +1,39 @@
+﻿module LR9_7
+
+open System
+open System.Windows.Forms
+open System.Windows.Forms
+open UI
+
+type StartupForm() as this = 
+    inherit Form1()
+    do
+        let cmbbx = this.comboBox2
+        let combolist = [("Январь", "Зима");("Февраль", "Зима");
+                         ("Март", "Весна");("Апрель", "Весна");
+                         ("Май", "Весна");("Июнь", "Лето");
+                         ("Июль", "Лето");("Август", "Лето");
+                         ("Сентябрь", "Осень");("Октябрь", "Осень");
+                         ("Ноябрь", "Осень");("Декабрь", "Зима")]
+        cmbbx.DataSource <- new BindingSource(combolist, null)
+        cmbbx.DisplayMember <- "Item1"
+        
+        this.button1.Click.Add <| fun _ ->
+                                        let parse (str:string) =
+                                            let u = str.Trim ('(', ')',' ')
+                                            let T = u.Split ','
+                                            (T.[0], T.[1])
+                                        if cmbbx.SelectedItem <> null then
+                                            match parse (cmbbx.SelectedItem.ToString()) with
+                                            | (_, b) -> MessageBox.Show(b)  
+                                        else
+                                            MessageBox.Show("Выберите Месяц!")
+                                        |> ignore
+
+[<EntryPoint; STAThread>]
+let main argv =
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault(false)
+    use form = new StartupForm()
+    Application.Run(form)
+    0
---
+++ LR9/LR9.7/UI/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь"});
+            this.comboBox2.Location = new System.Drawing.Point(66, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Чё за время года?";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 181);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.ComboBox comboBox2;
+        protected System.Windows.Forms.Button button1;
+    }
+}
+
